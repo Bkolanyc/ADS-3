035bb0d93a701e567f03794e01aba7a73c49dd74
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-03-07 12:54:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,11 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+        int h = 0;
+        for (int j = 0; j <= size; j++) {
+            if (*(arr + j) == value)
+                m += 1;
+        }
+        return h;
   return 0; // если ничего не найдено
 }
